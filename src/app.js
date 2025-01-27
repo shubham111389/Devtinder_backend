a94a42112ec7cd7c1c5451a47ec03ad7765c7fcb
shubham annpurne <shubhamannpurne1029@gmail.com>
2025-01-28 00:12:19 +0530

    created a express server

--- src/app.js
+++ src/app.js
@@ -2,10 +2,27 @@
 const express = require("express"); 
 
 const app = express();
+app.use(express.json());
 
-app.use("/",( req, res)=>{
-    res.send("server starting...");
-})
+
+app.post("/user/:id", async (req, res) => {
+    console.log(req.params);
+    console.log( req.query) // Logs the route parameters
+    res.send("Data successfully saved to the database!");
+});
+
+app.post("/user", async (req, res) => {
+    console.log(req.body);
+    console.log( req.params)
+    console.log( req.query)
+    // saving data to DB
+    res.send("Data successfully saved to the database!");
+  });
+app.get("/user", (req, res) => {
+    res.send({ firstName: "shubham", lastName: "annpurne" });
+  });
+
+  
 
 app.listen( '7000', ()=>{
     console.log("server listening on 7000 port");
