d5a0b3ca35b79b6fa46173e8f7758e9d1998bb4c
shubham annpurne <shubhamannpurne1029@gmail.com>
2025-05-07 23:16:16 +0530

    added  schema validation !! , and also add the api for delete user and update the used data !!

--- src/app.js
+++ src/app.js
@@ -6,18 +6,18 @@ const User = require("./models/user");
 app.use(express.json());
 
 app.post("/signup", async (req, res) => {
- 
+  //   Creating a new instance of the User model
   const user = new User(req.body);
 
   try {
     await user.save();
-    res.send("User Added successfully!",user);
+    res.send("User Added successfully!");
   } catch (err) {
     res.status(400).send("Error saving the user:" + err.message);
   }
 });
 
-
+// Get user by email
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailId;
 
@@ -48,6 +48,35 @@ app.get("/feed", async (req, res) => {
     res.send(users);
   } catch (err) {
     res.status(400).send("Something went wrong ");
+  }
+});
+
+// Detele a user from the database
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    //const user = await User.findByIdAndDelete(userId);
+
+    res.send("User deleted successfully");
+  } catch (err) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+// Update data of the user
+app.patch("/user", async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
+      returnDocument: "after",
+      runValidators: true,
+    });
+    console.log(user);
+    res.send("User updated successfully");
+  } catch (err) {
+    res.status(400).send("UPDATE FAILED:" + err.message);
   }
 });
 
