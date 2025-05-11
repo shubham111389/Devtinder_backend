68b84993f15d3b0a3bc6114f419944ee27464c79
shubham annpurne <shubhamannpurne1029@gmail.com>
2025-05-11 23:06:39 +0530

    added logout api !!

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,36 +1,66 @@
-const validator = require("validator");
+const express = require("express");
+const authRouter = express.Router();
 
-const validateSignUpData = (req) => {
-  const { firstName, lastName, emailId, password } = req.body;
-  if (!firstName || !lastName) {
-    throw new Error("Name is not valid!");
-  } else if (!validator.isEmail(emailId)) {
-    throw new Error("Email is not valid!");
-  } else if (!validator.isStrongPassword(password)) {
-    throw new Error("Please enter a strong Password!");
+const { validateSignUpData } = require("../utils/validation");
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
+
+authRouter.post("/signup", async (req, res) => {
+  try {
+    // Validation of data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+    console.log(passwordHash);
+
+    //   Creating a new instance of the User model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
+    await user.save();
+    res.send("User Added successfully!");
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
   }
-};
+});
 
-const validateEditProfileData = (req) => {
-  const allowedEditFields = [
-    "firstName",
-    "lastName",
-    "emailId",
-    "photoUrl",
-    "gender",
-    "age",
-    "about",
-    "skills",
-  ];
+authRouter.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
 
-  const isEditAllowed = Object.keys(req.body).every((field) =>
-    allowedEditFields.includes(field)
-  );
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("Invalid credentials");
+    }
+    const isPasswordValid = await user.validatePassword(password);
 
-  return isEditAllowed;
-};
+    if (isPasswordValid) {
+      const token = await user.getJWT();
 
-module.exports = {
-  validateSignUpData,
-  validateEditProfileData,
-};
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 8 * 3600000),
+      });
+      res.send("Login Successful!!!");
+    } else {
+      throw new Error("Invalid credentials");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+authRouter.post("/logout", async (req, res) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+  });
+  res.send("Logout Successful!!");
+});
+
+module.exports = authRouter;
